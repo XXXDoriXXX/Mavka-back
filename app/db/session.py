c22f7b1c071cecd57085a791d995eaee5ea5835a
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-03-31 09:29:48 +0300

    add config file

--- app/db/session.py
+++ app/db/session.py
@@ -3,9 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+from app.config import appConfig
+
+DATABASE_URL = appConfig.database.database_connection_string
+
 Base = declarative_base()
 
-engine = create_engine(os.environ["DATABASE_CONNECTION_STRING"], echo=True, future=True)
+engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def get_db():
