a80cd4a85faf45f740b3b8f6297c7d8e48bf4f6d
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-03-31 09:22:52 +0300

    make sync engine

--- app/db/session.py
+++ app/db/session.py
@@ -1,13 +1,16 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
 Base = declarative_base()
 
-engine = create_async_engine(os.environ["DATABASE_CONNECTION_STRING"], future=True, echo=True)
-SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_engine(os.environ["DATABASE_CONNECTION_STRING"], echo=True, future=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-async def get_db():
-    async with SessionLocal() as session:
-        yield session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
