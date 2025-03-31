53e8e46e230e7c0212eefe6664e12d182e979520
tolia k <tolia.kohan@gmail.com>
2025-03-31 11:31:06 +0300

    add routes for group and speciality

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from app.routers import auth
+from app.routers import auth, groups, specialities
 
 app = FastAPI()
 
 app.include_router(auth.router)
-
+app.include_router(groups.router)
+app.include_router(specialities.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
---
+++ app/routers/specialities.py
@@ -0,0 +1,49 @@
+# app/api/speciality.py
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.models import Speciality
+from app.schemas.speciality import SpecialityCreate, SpecialityResponse
+
+router = APIRouter(prefix="/specialities", tags=["specialities"])
+
+@router.post("/", response_model=SpecialityResponse)
+def create_speciality(speciality: SpecialityCreate, db: Session = Depends(get_db)):
+    db_speciality = Speciality(name=speciality.name)
+    db.add(db_speciality)
+    db.commit()
+    db.refresh(db_speciality)
+    return db_speciality
+
+@router.get("/", response_model=list[SpecialityResponse])
+def get_specialities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    db_specialities = db.query(Speciality).offset(skip).limit(limit).all()
+    return db_specialities
+
+@router.get("/{speciality_id}", response_model=SpecialityResponse)
+def get_speciality(speciality_id: int, db: Session = Depends(get_db)):
+    db_speciality = db.query(Speciality).filter(Speciality.id == speciality_id).first()
+    if db_speciality is None:
+        raise HTTPException(status_code=404, detail="Speciality not found")
+    return db_speciality
+
+@router.put("/{speciality_id}", response_model=SpecialityResponse)
+def update_speciality(speciality_id: int, speciality: SpecialityCreate, db: Session = Depends(get_db)):
+    db_speciality = db.query(Speciality).filter(Speciality.id == speciality_id).first()
+    if db_speciality is None:
+        raise HTTPException(status_code=404, detail="Speciality not found")
+
+    db_speciality.name = speciality.name
+    db.commit()
+    db.refresh(db_speciality)
+    return db_speciality
+
+@router.delete("/{speciality_id}", response_model=SpecialityResponse)
+def delete_speciality(speciality_id: int, db: Session = Depends(get_db)):
+    db_speciality = db.query(Speciality).filter(Speciality.id == speciality_id).first()
+    if db_speciality is None:
+        raise HTTPException(status_code=404, detail="Speciality not found")
+
+    db.delete(db_speciality)
+    db.commit()
+    return db_speciality
